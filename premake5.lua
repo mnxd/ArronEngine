f164f0985c848225c2748d7713ea2b0754993810
Arron <1021678461@qq.com>
2023-07-03 22:42:25 +0800

    update premake file

--- premake5.lua
+++ premake5.lua
@@ -28,6 +28,7 @@ project "ArronEngine"
     location "ArronEngine"
     kind "SharedLib"
     language "C++"
+    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -59,7 +60,6 @@ project "ArronEngine"
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines
@@ -76,23 +76,24 @@ project "ArronEngine"
 
     filter "configurations:Debug"
         defines "AE_DEBUG"
-        buildoptions "/MDd"
+        runtime "Debug"
         symbols "On"
 
     filter "configurations:Release"
         defines "AE_RELEASE"
-        buildoptions "/MD"
+         runtime "Release"
         optimize "On"
         
     filter "configurations:Dist"
         defines "AE_DIST"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
 project "Sandbox"
     location "Sandbox"
     kind "ConsoleApp"
     language "C++"
+    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -117,7 +118,6 @@ project "Sandbox"
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines
@@ -127,15 +127,15 @@ project "Sandbox"
 
     filter "configurations:Debug"
         defines "AE_DEBUG"
-        buildoptions "/MDd"
+        runtime "Debug"
         symbols "On"
 
     filter "configurations:Release"
         defines "AE_RELEASE"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
         
     filter "configurations:Dist"
         defines "AE_DIST"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
